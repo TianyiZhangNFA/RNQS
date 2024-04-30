3ae1914d4c37c4e66dfda41f234ddb7a926048b9
TianyiZhangNFA <112492226+TianyiZhangNFA@users.noreply.github.com>
2024-04-30 11:33:40 -0700

    Delete py_packages directory

--- py_packages/[exp]nqs_grv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# NQS vs Grover
-
-python [exp]nqs_grover.py --outDIR ./ --pp 8 --const_num_itr 3 --kk 9 --gamma 0.5 --ncores 10 --nrep 100  --seed1 5
-
-python [exp]nqs_grover.py --outDIR ./ --pp 9 --const_num_itr 3 --kk 9 --gamma 0.5 --ncores 10 --nrep 100  --seed1 5
-
-python [exp]nqs_grover.py --outDIR ./ --pp 10 --const_num_itr 3 --kk 9 --gamma 0.5 --ncores 10 --nrep 100  --seed1 5
